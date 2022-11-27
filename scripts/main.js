9daa0cb3b80c0b1062ca2ed32fd8a4cad6fdf3f8
igorviniciussantana <igor.santana@estudante.ifms.edu.br>
2022-11-27 11:27:18 -0400

    feat: add map function

--- scripts/main.js
+++ scripts/main.js
@@ -14,8 +14,9 @@ const clearLocate = document.getElementById('clear')
 const map = document.getElementById('map')
 
 const updateMap = () =>{
+
     map.insertAdjacentHTML('afterbegin', `
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3737.1102780747233!2d${-22.2510338}!3d${-53.3507573}!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9486e5c284bf66a1%3A0x36b345af23d39966!2sR.%20Pasteur%2C%20699-601%20-%20Vila%20Piratininga%2C%20Campo%20Grande%20-%20MS%2C%2079081-070!5e0!3m2!1spt-BR!2sbr!4v1668632665607!5m2!1spt-BR!2sbr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    <iframe src="http://maps.google.com/maps?q=${initialPosition.coords.latitude},${initialPosition.coords.longitude}&z=16&output=embed" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     `)
 }
 
